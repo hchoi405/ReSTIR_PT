7220ced0bdb6412307e5d745b14317b602712b8a
Hajin Choi <hchoi405@gmail.com>
2024-04-02 20:41:12 +0900

    Roll-back to original radius/number of reuse

--- Source/RenderPasses/ReSTIRPTPass/ReSTIRPTPass.h
+++ Source/RenderPasses/ReSTIRPTPass/ReSTIRPTPass.h
@@ -159,8 +159,8 @@ private:
     PathReusePattern                mPathReusePattern = PathReusePattern::NRooksShift;
     uint32_t                        mSmallWindowRestirWindowRadius = 2;
     int                             mSpatialNeighborCount = 3;
-    float                           mSpatialReuseRadius = 5.f;
-    int                             mNumSpatialRounds = 4;
+    float                           mSpatialReuseRadius = 20.f;
+    int                             mNumSpatialRounds = 1;
 
     bool                            mEnableTemporalReprojection = true;
     bool                            mFeatureBasedRejection = true;
