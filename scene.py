52d555145afffc3c08dd47bcdb9a740fe72d8ad4
Hajin Choi <hchoi405@gmail.com>
2024-08-27 20:39:32 +0900

    minor

--- scene.py
+++ scene.py
@@ -8,20 +8,15 @@ FBX_PATH = f"{FALCOR_DIR}/fbxscenes/"
 FBX_REMOTE_PATH = f"{NAS_DIR}/fbxscenes/"
 
 defs = {
-    # "BistroExteriorDoF": {'file': ORCA_PATH + "Bistro/BistroExteriorDoF.pyscene", 'anim':[1400, 1500]},
-    # "BistroExterior2": {'file': ORCA_PATH + "Bistro/BistroExterior.pyscene", 'anim':[1400, 1700]},
+    "BistroExterior2": {'file': ORCA_PATH + "Bistro/BistroExterior.pyscene", 'anim':[1400, 1700]},
     # "BistroExteriorDynamic": {'file': ORCA_PATH + "BistroExterior_dynamic_camera_flipped/BistroExterior_dynamic.pyscene", 'anim':[0, 300]},
     # "staircase" : {'file': FBX_PATH + "staircase/staircase.pyscene", 'anim':[0, 300]},
     # "EmeraldSquare2" : {'file': ORCA_PATH + "EmeraldSquare/EmeraldSquare_Day.pyscene", 'anim':[1700, 2000]},
-
-    # "spaceship" : {'file': FBX_REMOTE_PATH + "spaceship/spaceship.pyscene", 'anim':[0, 300]},
-    # "modernhall" : {'file': FBX_REMOTE_PATH + "modernhall/modernhall.pyscene", 'anim':[100, 101]},
-    # "kitchenware" : {'file': FBX_REMOTE_PATH + "kitchenware/kitchenware.pyscene", 'anim':[0, 300]},
-    # "terrazzo-kitchen" : {'file': FBX_REMOTE_PATH + "terrazzo-kitchen/terrazzo-kitchen.pyscene", 'anim':[0, 300]},
-    "kitchen5" : {'file': FBX_REMOTE_PATH + "kitchen5/kitchen5.pyscene", 'anim':[0, 300]},
+    # "musicroom22" : {'file': FBX_REMOTE_PATH + "musicroom22/musicroom22.pyscene", 'anim':[0, 300]},
 }
 
 considered = {
+    # "BistroExteriorDoF": {'file': ORCA_PATH + "Bistro/BistroExteriorDoF.pyscene", 'anim':[1400, 1500]},
     # "classroom" : {'file': FBX_PATH + "classroom/classroom.pyscene", 'anim':[0, 300]},
     # "BistroExterior1": {'file': ORCA_PATH + "Bistro/BistroExterior.pyscene", 'anim':[0, 600]},
     # "BistroExterior3": {'file': ORCA_PATH + "Bistro/BistroExterior.pyscene", 'anim':[3600, 4200]},
